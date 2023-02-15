439051b176aaedcf86988db03c48fa10414b8562
jordanleeprogram <jordanleedignan@gmail.com>
2023-02-15 16:16:35 +0000

    javascript nav bar

--- app.js
+++ app.js
@@ -6,3 +6,14 @@ function test() {
 }
 
 				
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-500px";
+  }
+  prevScrollpos = currentScrollPos;
+}
